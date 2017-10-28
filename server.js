ec1fbbaa440a80968a640f5a1cdf2ead53ab03d9
Vasiliy Shubko <flit77@gmail.com>
2017-10-29 00:56:28 +0200

    handling post request with hapi

---
+++ server.js
@@ -0,0 +1,47 @@
+const Hapi = require('hapi');
+const debug = require('debug');
+
+const dLog = debug('log');
+const dError = debug('error');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: (request, reply) => reply('hello world')
+});
+
+// Add the route
+server.route({
+  method: ['PUT', 'POST'],
+  path: '/tweet/add',
+  handler: (request, reply) => {
+    const { tweet, latitude, longitude } = request.payload;
+    if (!tweet) {
+      const errorMessage = 'tweet param missed';
+      dError(errorMessage);
+      return reply({
+        error: errorMessage
+      }).code(400);
+    }
+    dLog('request.params: ', request.payload);
+    return reply(
+      `tweet add action: post action. tweet: ${tweet}, latitude: ${latitude}, longitude: ${longitude}`
+    );
+  }
+});
+
+// Start the server
+server.start(err => {
+  if (err) {
+    throw err;
+  }
+  dLog('Server running at:', server.info.uri);
+});
