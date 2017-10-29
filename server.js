0246693a5c07672935e4a9cab5330add7e68418f
Vasiliy Shubko <flit77@gmail.com>
2017-10-29 16:03:57 +0100

    websocket added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Hapi = require('hapi');
 const debug = require('debug');
+const WebSocketServer = require('ws').Server;
 
 const dLog = debug('log');
 const dError = debug('error');
@@ -11,12 +12,34 @@ server.connection({
   port: 8000
 });
 
+const wss = new WebSocketServer({ port: 8081 });
+wss.on('connection', ws => {
+  dLog('websocket connection open');
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send(
+      JSON.stringify({
+        msg1: 'yo, im msg 1'
+      })
+    );
+
+    setTimeout(() => {
+      ws.send(
+        JSON.stringify({
+          msg2: 'yo, im a delayed msg 2'
+        })
+      );
+    }, 1000);
+  }
+});
+
 // Add the route
 server.route({
   method: ['PUT', 'POST'],
   path: '/tweet/add',
   handler: (request, reply) => {
     const { tweet, latitude, longitude } = request.payload;
+
     if (!tweet) {
       const errorMessage = 'tweet param missed';
       dError(errorMessage);
@@ -24,7 +47,21 @@ server.route({
         error: errorMessage
       }).code(400);
     }
+
     dLog('request.params: ', request.payload);
+
+    wss.on('connection', ws => {
+      dLog('websocket is cconnected');
+
+      if (ws.readyState === ws.OPEN) {
+        ws.send(
+          JSON.stringify({
+            tweet: `new tweet: ${tweet}, latitude: ${latitude}, longitude: ${longitude}`
+          })
+        );
+      }
+    });
+
     return reply(
       `tweet add action: post action. tweet: ${tweet}, latitude: ${latitude}, longitude: ${longitude}`
     );
