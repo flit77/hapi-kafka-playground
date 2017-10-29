8d94142e003869390fed2a725039c3a5cd1fb25f
Vasiliy Shubko <flit77@gmail.com>
2017-10-29 16:04:28 +0100

    websocket removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const Hapi = require('hapi');
 const debug = require('debug');
-const WebSocketServer = require('ws').Server;
 
 const dLog = debug('log');
 const dError = debug('error');
@@ -10,27 +9,6 @@ const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
   port: 8000
-});
-
-const wss = new WebSocketServer({ port: 8081 });
-wss.on('connection', ws => {
-  dLog('websocket connection open');
-
-  if (ws.readyState === ws.OPEN) {
-    ws.send(
-      JSON.stringify({
-        msg1: 'yo, im msg 1'
-      })
-    );
-
-    setTimeout(() => {
-      ws.send(
-        JSON.stringify({
-          msg2: 'yo, im a delayed msg 2'
-        })
-      );
-    }, 1000);
-  }
 });
 
 // Add the route
@@ -49,18 +27,6 @@ server.route({
     }
 
     dLog('request.params: ', request.payload);
-
-    wss.on('connection', ws => {
-      dLog('websocket is cconnected');
-
-      if (ws.readyState === ws.OPEN) {
-        ws.send(
-          JSON.stringify({
-            tweet: `new tweet: ${tweet}, latitude: ${latitude}, longitude: ${longitude}`
-          })
-        );
-      }
-    });
 
     return reply(
       `tweet add action: post action. tweet: ${tweet}, latitude: ${latitude}, longitude: ${longitude}`
