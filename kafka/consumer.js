65fbb16317525cce234190c732c61a25695f20f3
Vasiliy Shubko <flit77@gmail.com>
2017-10-31 01:21:41 +0100

    pass data from consumer to websockets

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,9 +1,12 @@
 const debug = require('debug');
 const { HighLevelConsumer, Client } = require('kafka-node');
+const WebSocketServer = require('ws').Server;
 const tweetSchema = require('./tweet.schema');
 
 const dLog = debug('log');
 const dError = debug('error');
+
+const wss = new WebSocketServer({ port: 8081 });
 
 const client = new Client('localhost:2181');
 const topics = [
@@ -20,10 +23,18 @@ const options = {
 };
 const consumer = new HighLevelConsumer(client, topics, options);
 
-consumer.on('message', message => {
-  const buf = Buffer.from(message.value, 'binary');
-  const decodedMessage = tweetSchema.fromBuffer(buf.slice(0));
-  dLog(decodedMessage);
+wss.on('connection', ws => {
+  dLog('ws connection opened');
+  consumer.on('message', message => {
+    dLog('kafka message received');
+    const buf = Buffer.from(message.value, 'binary');
+    const decodedMessage = tweetSchema.fromBuffer(buf.slice(0));
+    dLog(decodedMessage);
+    dLog('websocket connection open');
+    if (ws.readyState === ws.OPEN) {
+      ws.send(JSON.stringify(decodedMessage));
+    }
+  });
 });
 
 consumer.on('error', err => {
